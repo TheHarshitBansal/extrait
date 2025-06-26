87189da58ed35a2914c58f6e9f754c196e33c865
Harshit Bansal <harshit.bansal1236@gmail.com>
2025-06-27 00:24:43 +0530

    Implemented UploadThing

--- src/components/upload/UploadForm.tsx
+++ src/components/upload/UploadForm.tsx
@@ -2,8 +2,10 @@
 
 import React from "react";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { z } from "zod";
+import { useUploadThing } from "@/utils/uploadthing";
+import { toast } from "sonner";
+import { Input } from "../ui/input";
 
 const fileSchema = z.object({
   file: z
@@ -21,7 +23,23 @@ const fileSchema = z.object({
 });
 
 const UploadForm = () => {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const { startUpload, isUploading } = useUploadThing("pdfUploader", {
+    onClientUploadComplete: (res) => {
+      if (res && res.length > 0) {
+        toast.success("PDF uploaded successfully!");
+      } else {
+        toast.error("Failed to upload PDF.");
+      }
+    },
+    onUploadProgress: () => {
+      toast("Uploading PDF...");
+    },
+    onUploadError: (error) => {
+      toast.error(`Upload failed: ${error.message}`);
+    },
+  });
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
     const file = formData.get("file") as File;
@@ -30,18 +48,32 @@ const UploadForm = () => {
     const result = fileSchema.safeParse({ file });
 
     if (!result.success) {
-      console.log(
+      toast.error(
         result.error.flatten().fieldErrors.file?.[0] ?? "Invalid file"
       );
       return;
     }
+
+    const response = await startUpload([file]);
+    if (!response || response.length === 0) {
+      toast.error("Failed to upload PDF.");
+      return;
+    }
+
+    // toast.loading("Hang On! While our AI is processing your PDF 📄", {
+    //   richColors: true,
+    // });
   };
   return (
     <div className="flex flex-col gap-8 w-full max-w-2xl mx-auto">
       <form className="flex flex-col gap-6" onSubmit={handleSubmit}>
         <div className="flex justify-end items-center gap-1.5">
           <Input type="file" id="file" name="file" accept="application/pdf" />
-          <Button type="submit" className="cursor-pointer">
+          <Button
+            type="submit"
+            className="cursor-pointer"
+            disabled={isUploading}
+          >
             Upload PDF
           </Button>
         </div>
