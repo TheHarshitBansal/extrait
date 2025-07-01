71419dc0bee3c592eb16e325927eb29beb285613
Harshit Bansal <harshit.bansal1236@gmail.com>
2025-07-02 02:02:37 +0530

    updated clerk config

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,20 +5,19 @@ const isProtectedRoute = createRouteMatcher(['/dashboard/(.*)', '/summary/(.*)',
 
 export default clerkMiddleware(async (auth, req) => {
   try{
-
-    if(isProtectedRoute(req)) await auth.protect()
+      if(isProtectedRoute(req)) await auth.protect()
     } catch (error) {
       console.error('Error during authentication:', error);
       return new Response('Authentication failed', { status: 401 });
     }
 });
 
-export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//     // Always run for API routes
+//     '/(api|trpc)(.*)',
+//   ],
   
-};
+// };
